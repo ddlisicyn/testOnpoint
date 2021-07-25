7da8d0777f35982c048c84cfb5337024b76ebae3
Daniil <ddlisicyn@mail.ru>
2021-07-26 01:25:55 +0500

    second commit: 2nd slide, sperms animation, not working scroll, 3rd slide started

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 const firstSlideButton = document.querySelector('.first-slide__button'),
       main = document.querySelector('.main'),
       header = document.querySelector('.main__header'),
-      footer = document.querySelector('.main__footer');
+      footer = document.querySelector('.main__footer'),
+      sperms = document.querySelectorAll('.second-slide__sperms img'),
+      scrollerButton = document.querySelector('.description-scroller__scroll');
 
-let touchStart = 0,
-    touchPosition = 0,
-    sensitivity = 20,
-    x = 0;
+let touchStartX = 0,
+    touchPositionX = 0,
+    touchStartY = 0,
+    touchPositionY = 0,
+    sensitivity = 100,
+    x = 0,
+    currentPosition = 0,
+    trigger = 1;
 
 firstSlideButton.addEventListener('click', () => {
-    x -= 1024;
+    x = -1024;
     nextSlide(x, main);
     prevSlide(-x, header);
     prevSlide(-x, footer);
+    spermsRotation();
 });
 
 header.addEventListener('click', () => {
@@ -36,12 +43,11 @@ main.addEventListener('touchcancel', function(e) {
 });
 
 function TouchStart(e) {
-    touchStart = e.changedTouches[0].clientX;
-    touchPosition = touchStart.x;
+    touchStartX = e.changedTouches[0].clientX;
 }
 
 function TouchMove(e) {
-    touchPosition = e.changedTouches[0].clientX;
+    touchPositionX = e.changedTouches[0].clientX;
 }
 
 function TouchEnd(e) {
@@ -52,6 +58,7 @@ function TouchEnd(e) {
         nextSlide(x, main);
         prevSlide(-x, header);
         prevSlide(-x, footer);
+        spermsRotation();
     } else if (resultOfAction == 'Swipe Right' && x != 0) {
         x += 1024;
         prevSlide(x, main);
@@ -61,7 +68,7 @@ function TouchEnd(e) {
 }
 
 function CheckAction() {
-    let d = touchStart - touchPosition,
+    let d = touchStartX - touchPositionX,
         msg = '';
 
    	if(Math.abs(d) > sensitivity) {
@@ -84,3 +91,40 @@ function prevSlide(x, element) {
     element.style = `transform: translateX(${x}px);
                      transition: .5s;`;
 }
+
+function spermsRotation() {
+    if (trigger == 1) {
+        trigger--;
+        sperms.forEach(item => {
+            item.style = `animation-name: fade;`;
+            let time = window.getComputedStyle(item).animationDuration;
+            setTimeout(() => {
+                item.style = `transform: translate3d(15px, 15px, 0);`;
+            }, parseFloat(time) * 1000);
+        });
+    }
+}
+
+scrollerButton.addEventListener('touchstart', function(e) {
+    TouchStart2(e);
+});
+scrollerButton.addEventListener('touchmove', function(e) {
+    TouchMove2(e);
+});
+
+function TouchStart2(e) {
+    touchStartY = e.changedTouches[0].clientY;
+    console.log(`1nd: ${touchStartY}`);  
+}
+
+function TouchMove2(e) {
+    touchPositionY = e.changedTouches[0].clientY;
+    console.log(`2nd: ${touchPositionY}`);
+    let d = touchPositionY - touchStartY;
+
+    if (d < 360 && d > 0) {
+        scrollerButton.style = `transform: translateY(${d}px)`;
+        currentPosition = +scrollerButton.style.transform.match(/\d+/g).join('.');
+    }
+}
+
